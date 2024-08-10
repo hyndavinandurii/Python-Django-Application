376e00ecf69927024469ca3091c42f18113d5e1b
ratnakottara <ratnakottara@gmail.com>
2024-08-10 23:39:10 +0530

    Updated

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -1,20 +1,25 @@
+# Define the data source for availability zones
+data "aws_availability_zones" "available" {}
+
+# Define a public subnet
 resource "aws_subnet" "public" {
-  count = 2
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.1.${count.index + 1}.0/24"
+  count             = 2
+  vpc_id            = aws_vpc.main.id
   availability_zone = element(data.aws_availability_zones.available.names, count.index)
+  cidr_block        = element(var.public_subnet_cidrs, count.index)
   map_public_ip_on_launch = true
   tags = {
-    Name = "public-subnet-${count.index + 1}"
+    Name = "public-subnet-${count.index}"
   }
 }
 
+# Define a private subnet
 resource "aws_subnet" "private" {
-  count = 2
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.2.${count.index + 1}.0/24"
+  count             = 2
+  vpc_id            = aws_vpc.main.id
   availability_zone = element(data.aws_availability_zones.available.names, count.index)
+  cidr_block        = element(var.private_subnet_cidrs, count.index)
   tags = {
-    Name = "private-subnet-${count.index + 1}"
+    Name = "private-subnet-${count.index}"
   }
 }
