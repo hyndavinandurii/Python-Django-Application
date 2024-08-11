024824b0e7ff958356fc0bb2bcc10cad1f334706
ratnakottara <ratnakottara@gmail.com>
2024-08-11 17:40:24 +0530

    Updated

--- terraform/ecs_task_definition.tf
+++ terraform/ecs_task_definition.tf
@@ -16,6 +16,6 @@ resource "aws_ecs_task_definition" "django_task" {
   }])
   network_mode             = "bridge"
   requires_compatibilities = ["EC2"]
-  execution_role_arn       = module.iam_roles.ecs_execution_role_arn
-  task_role_arn            = module.iam_roles.ecs_task_role_arn
+  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
 }
